aba0e1976a3abce96f9e0cd4a4cd7c31fec7012f
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-03 02:49:39 +0700

    Update 2.4.go

--- 2.4.go
+++ 2.4.go
@@ -12,11 +12,12 @@ func main() {
 	for i := 0; i < 10000; i++ {
 		a += "a"
 	}
-fmt.Prinln("a", time.Since(startA))
-
-starB := time.Now()
-var b strings. Builder
-for i := 0; i < 10000; i++{
-	b.WriteString("b")
-}
+	fmt.Prinln("a", time.Since(startA))
+	
+    starB := time.Now()
+    var b strings. Builder
+    for i := 0; i < 10000; i++{
+	    b.WriteString("b")
+    }
+    fmt.Prinln("b",time.Since(startB))
 }
