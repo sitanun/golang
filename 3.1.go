532845976c4190938a3557266a121a35de2ea5e7
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-10 16:56:53 +0700

    Update 3.1.go

--- 3.1.go
+++ 3.1.go
@@ -5,14 +5,14 @@ import "fmt"
 func main() {
 	fmt.Printlf("My name is %s, I am %d years old\n", "sitanun", 19)
 
-	fmt.Printlf("type = %T \n", 2.8989898989)
-	fmt.Printlf("pi = %f \n",2.8989898989)
-	fmt.Printlf("pi = %2.2f \n",2.8989898989)
-	fmt.Printlf("pi = %9.f \n", 2.8989898989)
-	fmt.Printlf("pi = %-9.f \n", 2.8989898989)
-	fmt.Printlf("pi = %09.f \n", 2.8989898989)
-	fmt.Printlf("pi = %09.2f \n", 2.8989898989)
-	fmt.Printlf("pi = %E \n", 2.8989898989)
+	fmt.Printlf("type = %T \n", 2.89898989898)
+	fmt.Printlf("pi = %f \n",2.89898989898)
+	fmt.Printlf("pi = %2.2f \n",2.89898989898)
+	fmt.Printlf("pi = %9.f \n", 2.89898989898)
+	fmt.Printlf("pi = %-9.f \n", 2.89898989898)
+	fmt.Printlf("pi = %09.f \n", 2.89898989898)
+	fmt.Printlf("pi = %09.2f \n", 2.89898989898)
+	fmt.Printlf("pi = %E \n", 2.89898989898)
 
 	fmt.Printlf("2 > 1 =%t \n", 2 > 1)
 	fmt.Printlf("20 (base 2) = %b \n", 20)
