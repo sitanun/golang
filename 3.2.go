3cd5bbc4c61989deb336dbb535b7fd34fa222734
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-10 17:27:47 +0700

    Update 3.2.go

--- 3.2.go
+++ 3.2.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	n, e := fmt.Print("Hello", "World",621, 126, "\n")
+	fmt.Print("number of bytes written :", n, "\n")
+	fmt.Print("write error encountered :", e, "\n")
 }
