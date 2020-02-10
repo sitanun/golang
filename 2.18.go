e4a9ae263367a128e650d255f08ce9f5161fac4a
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-10 15:59:52 +0700

    Update 2.18.go

--- 2.18.go
+++ 2.18.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 
-	fmt.Printf( "10 is of type %T\n", 20)
+	fmt.Printf( "20 is of type %T\n", 20)
 
-	fmt.Printf( "10 is of type %T\n", string(20))
+	fmt.Printf( "20 is of type %T\n", string(20) )
+
+	fmt.Printf( "20 is of type %T\n", float64(20) )
 }
