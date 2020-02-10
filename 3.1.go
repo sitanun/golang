4701593c1db694ccf8c19377cd3774c1acbdd6d7
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-10 17:03:27 +0700

    Update 3.1.go

--- 3.1.go
+++ 3.1.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printlf("My name is %s, I am %d years old\n", "sitanun", 19)
+	fmt.Printf("My name is %s, I am %d years old\n", "sitanun", 19)
 
-	fmt.Printlf("type = %T \n", 3.14159265359)
-	fmt.Printlf("pi = %f \n", 3.14159265359)
-	fmt.Printlf("pi = %2.2f \n", 3.14159265359)
-	fmt.Printlf("pi = %9.f \n", 3.14159265359)
-	fmt.Printlf("pi = %-9.f \n", 3.14159265359)
-	fmt.Printlf("pi = %09.f \n", 3.14159265359)
-	fmt.Printlf("pi = %09.2f \n", 3.14159265359)
-	fmt.Printlf("pi = %E \n", 3.14159265359)
+	fmt.Printf("type = %T \n", 3.14159265359)
+	fmt.Printf("pi = %f \n", 3.14159265359)
+	fmt.Printf("pi = %2.2f \n", 3.14159265359)
+	fmt.Printf("pi = %9.f \n", 3.14159265359)
+	fmt.Printf("pi = %-9.f \n", 3.14159265359)
+	fmt.Printf("pi = %09.f \n", 3.14159265359)
+	fmt.Printf("pi = %09.2f \n", 3.14159265359)
+	fmt.Printf("pi = %E \n", 3.14159265359)
 
-	fmt.Printlf("1 > 2 = %t \n", 1 > 2)
-	fmt.Printlf("20 (base 2) = %b \n", 20)
-	fmt.Printlf("20 (base 4) = %o \n", 20)
-	fmt.Printlf("20 (base 8) = %d \n", 20)
-	fmt.Printlf("20 (base 10) = %x \n", 20)
+	fmt.Printf("1 > 2 = %t \n", 1 > 2)
+	fmt.Printf("10 (base 2) = %b \n", 10)
+	fmt.Printf("10 (base 8) = %o \n", 10)
+	fmt.Printf("10 (base 10) = %d \n", 10)
+	fmt.Printf("10 (base 16) = %x \n", 10)
 }
