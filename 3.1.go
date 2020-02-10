b1e6740f989c167dba038abdf14f18deb484530b
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-10 16:42:38 +0700

    Update 3.1.go

--- 3.1.go
+++ 3.1.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
 	fmt.Printlf("My name is %s, I am %d years old\n", "sitanun", 19)
 
-	fmt.Printlf("type = %T \n" 2.8989898989)
+	fmt.Printlf("type = %T \n", 2.8989898989)
+	fmt.Printlf("pi = %f \n",2.8989898989)
 }
