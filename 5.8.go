c5a9b28188ca224b1efc60d1090f063d4b0bb982
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-11 22:23:04 +0700

    Update 5.8.go

--- 5.8.go
+++ 5.8.go
@@ -5,5 +5,9 @@ import "fmt"
 func main() {
 	alphabets := [2][3]string{{"a", "b", "c"}, {"A", "B", "C"}}
 	fmt.Prinln(alphabets)
-	fmt.Prinln(alphabets[0][1])
+	fmt.Prinln(alphabets[0][1]
+	numbers := [2][3][2]int{
+		
+	}
+	)
 }
