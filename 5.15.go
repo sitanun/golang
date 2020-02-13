7244d4d015b7bac53770b89906b4a970641969c8
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-13 11:50:01 +0700

    Update 5.15.go

--- 5.15.go
+++ 5.15.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main(){
 	names := []string{}
-	names := append(names, "AA", "BB", "CC", "DD", "EE")
+	names := append(names, "Aa", "Bb", "Cc", "Dd", "Ee")
 	fmt.Println(names)
 }
