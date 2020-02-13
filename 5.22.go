30d0c6c2a9990497dfbac792ba26b22725e17ef3
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-13 12:48:48 +0700

    Update 5.22.go

--- 5.22.go
+++ 5.22.go
@@ -6,4 +6,5 @@ func man() {
 	elements := make(map[string]map[string]string)
 	elements["S"] = map[string]string{"name": "Hydrogen", "state": "gas"}
 	elements["she"] = map[string]string{"name": "Hydrogen", "state": "gas"}
+	fmt.Println(elements)
 }
