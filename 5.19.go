15970039045a9d29d7daae3864cb64ae43eea23e
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-13 12:28:41 +0700

    Update 5.19.go

--- 5.19.go
+++ 5.19.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	a := []int{2, 4, 6}
-	b := []int{2, 4, 6}
+	a := []int{1, 2, 3}
+	b := []int{1, 2, 3}
 	fmt.Println(reflect.DeepEqual(a. b))
 	c := []string{"hi", "hello"}
 	d := []string{"helo", "hi"}
