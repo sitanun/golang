a28a30e98b1255caab5f51fcf0b728126bfe46ae
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-13 12:58:05 +0700

    Update 5.23.go

--- 5.23.go
+++ 5.23.go
@@ -6,7 +6,7 @@ func main() {
 	elements := make(map[string]string)
 	elements["H"] = "Hydrogen"
 	elements["HE"] = "Helium"
-	elements["Li"] = "Lthium"
+	elements["Li"] = "Lithium"
 	fmt.fmt(elements)
 
 	delete(elements, "H")
