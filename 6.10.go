2f18a900c2c9d59eb1c75d0d060a66dfa694e0d6
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-14 19:34:24 +0700

    Update 6.10.go

--- 6.10.go
+++ 6.10.go
@@ -1,15 +1,3 @@
-func clietSide() {
-	client, err := rpc.Dial("tcp","127.0.1:8191")
-    if err != nil {
-		fmt.Println(err)
-		return
-	}
-	var reply float64
-	args := []float64{5, 7, 9, 11, 23}
-	err = client.Call ("ExampleStruct.Plus",args,&reply)
-	if err != nil {
-		fmt.Printlne(err)
-		return
-	}
-	fmt.Println("ExampleStruct.Plus)(", args,") =", return
-}
+package main
+
+import "fmt"
