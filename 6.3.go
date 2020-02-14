2982f99887be091c25ab1b7f44fc64d31f16d6c1
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-14 17:33:14 +0700

    Update 6.3.go

--- 6.3.go
+++ 6.3.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main {
-	r := []bool{true, true, false, false, true, true, false, false}
+	r := []bool{true, true, false, false, true, true}
 	for _, v := rang r {
 		fmt.Printf("%t\n", v)
 	}
