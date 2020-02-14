7b1fc2bf0870cb6f5368344f38d950d70d299b9b
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-14 17:25:57 +0700

    Update 6.3.go

--- 6.3.go
+++ 6.3.go
@@ -1,19 +1,1 @@
 package main
-
-import "fmt"
-
-func sum(numbers ...int) int {
-	total := 0
-	for _, n := range numbers {
-		total = total + n
-	}
-    return total
-}
-
-func main() {
-	x := sum(1, 3, 5, 7, 9)
-	fmt.Println(x)
-
-	y := sum()
-	fmt.Println(y)
-}
