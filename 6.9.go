f3fb9ac7ec251ed4d6a934534c258e255d223c00
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-14 19:27:48 +0700

    Update 6.9.go

--- 6.9.go
+++ 6.9.go
@@ -1,13 +1,1 @@
-func serverSide() {
-	rpc.Register (new(ExamleStruct))
-	ln, _ := nrt.Listen("tcp","8191")
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			return
-		} else {
-			go rpc.ServeConn(conn)
-		}
-	}
-}
+package main
