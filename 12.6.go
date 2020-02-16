c4e1a73f68c7871be2bde22146d5a28f94504f6f
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-16 22:05:34 +0700

    Create 12.6.go

---
+++ 12.6.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	dir, err := os.Open(".")
+	if err != nil {
+		return
+	}
+	defer dir.Close()
+
+	fileInfos, err :=dir.Readdir(-1)
+	if err != nil {
+		return
+	}
+}
