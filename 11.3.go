d1bd0353218d1afc340e6438d2a068f73a7a4bd9
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-16 21:21:11 +0700

    Update 11.3.go

--- 11.3.go
+++ 11.3.go
@@ -8,5 +8,12 @@ import (
 func say(txt string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(time.Now(), ":", i, ":", txt)
+		time.Sleep(time.Millisecond)
 	}
+}
+func main() {
+	go say("Hello")
+	go say("Hi")
+	var input string
+	fmt.Scanln(&input)
 }
