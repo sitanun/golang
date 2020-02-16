c519da204877b1b4f49d71fbc11f8550f8b8a3bd
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-16 21:56:55 +0700

    Create 12.4.go

---
+++ 12.4.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	file, err := os.Create("myFile.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteString("Hello \n")
+	file.WriteString("i amFile.txt")
+}
