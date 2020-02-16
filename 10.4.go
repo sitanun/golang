acc5369cc8c716e9cd052e75a8a180cd9bcfef9e
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-16 13:59:24 +0700

    Update 10.4.go

--- 10.4.go
+++ 10.4.go
@@ -3,4 +3,8 @@ package main
 //find the average of a series of numbers
 func Average(numbers ...floa64) floa64 {
 	var total floa64
+	for _, v := range numbers {
+		total = total + v
+	}
+	return total / floa64(len(numbers))
 }
