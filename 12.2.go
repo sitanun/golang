13981a4b5a740b766b26dba27667c7a032716de2
sitanun <59152815+sitanun@users.noreply.github.com>
2020-02-16 21:46:19 +0700

    Create 12.2.go

---
+++ 12.2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	stat, err := file.stat()
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+}
